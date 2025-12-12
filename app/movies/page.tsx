510aae4464af7aaa2158577121c0af4e49d935f0
archanaprabhat <archanaprabhath2002@gmail.com>
2025-12-12 05:58:15 +0530

    Fix build errors: Add Suspense boundaries and sync local files

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from "react"
 import { getMovies } from "@/utils/fetchData"
 import MovieListing from "@/components/MovieListing"
 
 export default async function MoviesPage() {
   const movies = await getMovies()
-  return <MovieListing initialMovies={movies} />
+  return (
+    <Suspense>
+      <MovieListing initialMovies={movies} />
+    </Suspense>
+  )
 }
--- app/shows/page.tsx
+++ app/shows/page.tsx
@@ -1,7 +1,12 @@
+import { Suspense } from "react"
 import { getShows } from "@/utils/fetchData"
 import ShowListing from "@/components/ShowListing"
 
 export default async function ShowsPage() {
   const shows = await getShows()
-  return <ShowListing initialShows={shows} />
+  return (
+    <Suspense>
+      <ShowListing initialShows={shows} />
+    </Suspense>
+  )
 }
